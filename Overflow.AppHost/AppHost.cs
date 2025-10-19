d0ebc1c5f845e2f086163076bf754f778e57cf99
Onyekachi Chile <onyekachic@live.com>
2025-10-20 00:08:28 +0100

    End of sedtion 2

---
+++ Overflow.AppHost/AppHost.cs
@@ -0,0 +1,18 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var keycloak = builder.AddKeycloak("keycloak", 6001)
+    .WithDataVolume("keycloak-data");
+
+    //var postgres = builder.AddPostgres("postgres", port: 5438)
+    //.WithDataVolume("postgres-data")
+   // .WithPgAdmin();
+
+//var questionDb = postgres.AddDatabase("questionDb");
+
+//var questionService = builder.AddProject<Projects.QuestionService>("question-svc")
+    //.WithReference(keycloak)
+   // .WithReference(questionDb)
+  //  .WaitFor(keycloak)
+   // .WaitFor(questionDb);
+
+builder.Build().Run();

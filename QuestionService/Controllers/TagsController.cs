f0f9593fcf8a66035ed9ade88657affde523c6bf
Onyekachi Chile <onyekachic@live.com>
2025-11-02 19:49:09 +0000

    End of sedtion 3

---
+++ QuestionService/Controllers/TagsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuestionService.Data;
+using QuestionService.Models;
+
+namespace QuestionService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TagsController(QuestionDbContext db) : ControllerBase
+{
+    public async Task<ActionResult<IReadOnlyList<Tag>>> Get()
+    {
+        return await db.Tags.OrderBy(x => x.Name).ToListAsync();
+    }
+}
